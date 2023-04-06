005035b895063f0048e079ce07680bb593285441
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2023-04-06 21:03:05 +0200

    feat: add cors

--- graph-project/server/src/main.rs
+++ graph-project/server/src/main.rs
@@ -2,10 +2,38 @@ use std::{cell::RefCell, collections::HashMap, sync::Arc};
 
 use dotenv::dotenv;
 use map::dijkstra::WeightedNode;
-use rocket::{http::Status, launch, routes, serde::json::Json};
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::{Header, Status},
+    launch, routes,
+    serde::json::Json,
+    Request, Response,
+};
 
 #[macro_use]
 extern crate rocket;
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[get("/", format = "json")]
 async fn get_map() -> Result<Json<map::Map>, Status> {
@@ -31,5 +59,5 @@ async fn get_shortest_path_for_id(
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
-    rocket::build().mount("/", routes![get_map, get_shortest_path_for_id])
+    rocket::build().attach(CORS).mount("/", routes![get_map, get_shortest_path_for_id])
 }

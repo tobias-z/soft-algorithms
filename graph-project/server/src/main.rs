4024d72c8fb6d92479133d8abc584fa26a5dcffd
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2023-04-06 21:17:32 +0200

    fix: remove cors as nginx config was added to allow cors from own domain

--- graph-project/server/src/main.rs
+++ graph-project/server/src/main.rs
@@ -2,38 +2,10 @@ use std::{cell::RefCell, collections::HashMap, sync::Arc};
 
 use dotenv::dotenv;
 use map::dijkstra::WeightedNode;
-use rocket::{
-    fairing::{Fairing, Info, Kind},
-    http::{Header, Status},
-    launch, routes,
-    serde::json::Json,
-    Request, Response,
-};
+use rocket::{http::Status, launch, routes, serde::json::Json};
 
 #[macro_use]
 extern crate rocket;
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
 
 #[get("/", format = "json")]
 async fn get_map() -> Result<Json<map::Map>, Status> {
@@ -59,5 +31,5 @@ async fn get_shortest_path_for_id(
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
-    rocket::build().attach(CORS).mount("/", routes![get_map, get_shortest_path_for_id])
+    rocket::build().mount("/", routes![get_map, get_shortest_path_for_id])
 }
